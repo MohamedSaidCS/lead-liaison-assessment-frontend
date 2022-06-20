91ce869ebdca4f258ee124dbd6247e1b3a4aac83
Mohamed <darkmorning98@gmail.com>
2022-06-20 19:05:32 +0200

    TodoComponent implementation

--- src/components/TodoComponent.vue
+++ src/components/TodoComponent.vue
@@ -1,11 +1,46 @@
 <template>
-  todo
+  <div class="text-center mt-5" v-if="loading">Loading...</div>
+  <template v-else>
+    <div class="mt-5 d-flex justify-content-between align-items-center">
+      <h1>Todo #{{id}}</h1>
+      <router-link to="/todos" class="btn btn-primary">Back</router-link>
+    </div>
+    <div class="card text-center">
+      <div class="card-header">
+        Todo #{{todo.id}} - Details
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">{{todo.title}}</h5>
+        <p class="card-text">{{todo.description}}</p>
+        <router-link :to="`/todos/${todo.id}/edit`" class="btn btn-primary">Edit</router-link>
+      </div>
+      <div class="card-footer text-muted">
+        {{todo.created_at}}
+      </div>
+    </div>
+  </template>
 </template>
 
-<script>
-export default {
-  name: "TodoComponent"
+<script setup>
+import {ref} from "vue";
+import {useRoute} from 'vue-router';
+import axios from "axios";
+
+const route = useRoute();
+const id = ref(route.params.id);
+const todo = ref({});
+const loading = ref(false);
+
+const fetchTodo = async (id) => {
+  loading.value = true
+  const response = await axios.get(`http://127.0.0.1:8000/api/todos/${id}`);
+  todo.value = await response.data;
+  loading.value = false;
 }
+
+fetchTodo(id.value);
+
+
 </script>
 
 <style scoped>
