87e25d1eb5913cddf959827b2104787e364d9136
MohamedSaidCS <60986229+MohamedSaidCS@users.noreply.github.com>
2022-06-21 22:14:50 +0200

    Fix typo in HomeComponent

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -2,7 +2,7 @@
   <div class="mt-5 text-center">
     <h1 class="mb-5">Mohamed Said Mohamed Shaaban</h1>
     <h2 class="mb-5">Todo List CRUD Assessment - Lead Liaison</h2>
-    <h3>Click <router-link to="/todos">here</router-link> or use the navar to use the application.</h3>
+    <h3>Click <router-link to="/todos">here</router-link> or use the navbar to use the application.</h3>
   </div>
 </template>
 
